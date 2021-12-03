366a0841cd006bdc01c732d73ded74079b3cb262
leandroBGoncalves <leandrodebritogoncalves@gmail.com>
2021-12-03 17:11:18 -0400

    implementacao do modal de criacao de transactions

--- src/components/ModalInnerTransactions/index.jsx
+++ src/components/ModalInnerTransactions/index.jsx
@@ -1,14 +1,92 @@
-import { Dialog } from '@material-ui/core';
+import { useState } from "react";
+import {
+  Dialog,
+  FormControl,
+  FormControlLabel,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  Radio,
+  RadioGroup,
+  Switch,
+  TextField,
+} from "@material-ui/core";
 
-export default function ModalTransactions({open, handleClose}) {
-    return (
-        <Dialog
-        fullWidth
-        maxWidth="sm"
-        open={open}
-        onClose={handleClose}
-      >
+import styles from "./styleModal.module.scss";
 
-        </Dialog>
-    )
+export default function ModalTransactions({ open, handleClose }) {
+  const [amount, setAmount] = useState();
+  const [checked, setChecked] = useState(false);
+  const [titleAmount, setTitleAmount] = useState('');
+  const [type, setType] = useState('entrada')
+  const [dateCreated, setDateCreated] = useState()
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
+
+  return (
+    <Dialog fullWidth maxWidth="sm" open={open} onClose={handleClose}>
+      <div className={styles.TitleModal}>
+        <h4>
+          Inserir dados
+        </h4>
+        <h3 onClick={handleClose}>X</h3>
+      </div>
+      <div className={styles.ContainerModal}>
+        <TextField
+          className={styles.inputTitle}
+          fullWidth
+          value={titleAmount}
+          onChange={(e) => {
+            setTitleAmount(e.target.value)
+          }}
+          id="outlined-basic"
+          label="Titulo"
+          variant="outlined"
+        />
+        <FormControl fullWidth>
+          <InputLabel htmlFor="outlined-adornment-amount">Valor</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-amount"
+            value={amount}
+            onChange={(e) => {
+              setAmount(e.target.value);
+            }}
+            startAdornment={
+              <InputAdornment position="start">R$</InputAdornment>
+            }
+            label="Amount"
+          />
+        </FormControl>
+        <div className={styles.interruptors}>
+          <FormControlLabel
+            control={<Switch defaultChecked color="secondary" />}
+            label="Incluir parcelas"
+            checked={checked}
+            onChange={handleChange}
+          />
+          <FormControl component="fieldset">
+            <RadioGroup
+              aria-label="gender"
+              defaultValue="female"
+              name="radio-buttons-group"
+              value={type}
+              onChange={(e) => {
+                setType(e.target.value)
+              }}
+            >
+              <FormControlLabel value="entrada" control={<Radio />} label="Entrada" />
+              <FormControlLabel value="saida" control={<Radio />} label="Saida" />
+            </RadioGroup>
+          </FormControl>
+        </div>
+        <div className={styles.boxBTN}>
+          <button className={styles.BTN_SaveTransaction}>
+            Salvar
+          </button>
+        </div>
+      </div>
+    </Dialog>
+  );
 }
